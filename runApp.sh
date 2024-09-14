d0e5f9057dd8412d4533ec9bfd7cd295873930a2
Rohan Agrawal <rohan.agra49@gmail.com>
2024-09-14 10:45:08 -0700

    changed local path name

--- runApp.sh
+++ runApp.sh
@@ -1,3 +1,3 @@
 mvn clean package
-java --module-path /INSERT YOUR PATH TO/JavaFX22/lib --add-modules javafx.controls,javafx.fxml -jar target/BookStore-1.0-SNAPSHOT.jar
+java --module-path /Users/ragrwal/Downloads/JavaFX22/lib --add-modules javafx.controls,javafx.fxml -jar target/BookStore-1.0-SNAPSHOT.jar
 
