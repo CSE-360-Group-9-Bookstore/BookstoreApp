8d3d32017342461125d4653016e6d9abfd361a01
Rohan Agrawal <rohan.agra49@gmail.com>
2024-11-01 17:25:12 -0700

    added UUID as a field to users

--- src/main/java/bookstore/lib/User.java
+++ src/main/java/bookstore/lib/User.java
@@ -4,9 +4,11 @@ public class User{
 
         public String username;
         public String role;
+        public UUID user_uuid;
 
-        public User(String username, String role) {
+        public User(String username, String role, UUID user_uuid) {
             this.username = username;
             this.role = role;
+            this.user_uuid = user_uuid;
         }
     }
